5aa9cf3e6899eb70ff10353759838ec3cc3d1318
Alexander Mordas <alexandermordas@gmail.com>
2017-05-23 16:08:05 +0200

    changed colors

--- PHP/dynamicnav.php
+++ PHP/dynamicnav.php
@@ -1,33 +1,65 @@
-<style>
-    #menu {
-        display block;
-        background-color:#272822;
-        padding:10px;
-    }
-    #menu a {
-        background-color:#17a68a;
-        color:#ffffff;
-        font-weight:bold;
-        padding:5px;
-        text-decoration:none;
-        margin-right:5px;
-    }
-    #menu a:hover {
-        background-color:#1abc9c;
-    }
-</style>
-
-<?php
-$pages = array(
-    'index.php' => 'Home',
-    'about.php' => 'About',
-    'contact.php' => 'Contact'
-);
-?>
-<nav id="menu">
-    <?php
-    foreach($pages as $link => $title) {
-        echo '<a href="'.$link.'">'.$title.'</a>';
-     }
-    ?>
+<style>
+
+    #menu {
+
+        display block;
+
+        background-color:#272822;
+
+        padding:10px;
+
+    }
+
+    #menu a {
+
+        background-color:#a61733;
+
+        color:#ffffff;
+
+        font-weight:bold;
+
+        padding:5px;
+
+        text-decoration:none;
+
+        margin-right:5px;
+
+    }
+
+    #menu a:hover {
+
+        background-color:#bc1a3a;
+
+    }
+
+</style>
+
+
+
+<?php
+
+$pages = array(
+
+    'index.php' => 'Home',
+
+    'about.php' => 'About',
+
+    'contact.php' => 'Contact'
+
+);
+
+?>
+
+<nav id="menu">
+
+    <?php
+
+    foreach($pages as $link => $title) {
+
+        echo '<a href="'.$link.'">'.$title.'</a>';
+
+     }
+
+    ?>
+
 </nav>
